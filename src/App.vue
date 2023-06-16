b3a695ed3ddb9dc4e47324cdcf4c36338dcc22ab
shilik <ltshieh@gmail.com>
2023-06-16 16:26:36 +0800

    config: add ability to toggle to a third theme called cafe

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,22 @@ import { useColorMode } from "@vueuse/core";
 import HelloWorld from "./components/HelloWorld.vue";
 import IconMoon from "./components/iconMoon.vue";
 import IconSun from "./components/IconSun.vue";
-const colorMode = useColorMode();
+import IconCafe from "./components/IconCafe.vue";
+const colorMode = useColorMode({
+  modes: {
+    cafe: "cafe",
+    light: "light",
+    dark: "dark",
+  },
+});
 const switchTheme = () => {
-  colorMode.value = colorMode.value === "dark" ? "light" : "dark";
+  if (colorMode.value === "dark") {
+    colorMode.value = "cafe";
+  } else if (colorMode.value === "cafe") {
+    colorMode.value = "light";
+  } else {
+    colorMode.value = "dark";
+  }
 };
 </script>
 
@@ -19,7 +32,9 @@ const switchTheme = () => {
     >
       <IconSun v-if="colorMode==='light'" />
       <IconMoon v-if="colorMode==='dark'" />
+      <IconCafe v-if="colorMode==='cafe'" />
     </button>
+
   </header>
   <h1>{{ colorMode }}</h1>
   <a
@@ -49,7 +64,9 @@ const switchTheme = () => {
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-
+.cafe {
+  filter: sepia(0.9) hue-rotate(315deg) brightness(0.9);
+}
 .dark {
   background-color: #222;
   color: #fff;
---
+++ src/components/IconCafe.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M4 21v-2h16v2H4Zm4-4q-1.65 0-2.825-1.175T4 13V3h16q.825 0 1.413.588T22 5v3q0 .825-.588 1.413T20 10h-2v3q0 1.65-1.175 2.825T14 17H8Zm0-2h6q.825 0 1.413-.588T16 13V5H6v8q0 .825.588 1.413T8 15Zm10-7h2V5h-2v3Zm-7 2Z"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "MaterialSymbolsLocalCafeOutline",
+};
+</script>
